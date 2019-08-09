bad637d92d047a72664a8e487e940a05ae65ac49
insideoutzombie <david@web3devs.com>
2019-08-09 15:24:24 -0500

    adds functionality to index.js phase 1

--- index.js
+++ index.js
@@ -13,42 +13,28 @@
 var user = parseInt(prompt("Welcome to the magic Factory. Press '1' to count magic. Press '2' to use a magical calculator. Press 'q' to quit!"));
 
 if (user === 1) {
-  alert("success");
-  var num1 = parseInt(prompt("Please enter a number"));
-  for (var x = num1; x < 0; x++) {
+  alert("Welcome to level 1");
+  var kount = prompt("Would you like to count by ones? Press 'y' of yes ");
+  if (kount === 'y') {
+    var numb1 = parseInt(prompt("Enter a number bruh"));
 
+    for (var x = 1; x <= numb1; x++){
+      console.log(x);
+    }
+    x++;
   }
+}
 
-  alert(x);
-  x++;
-  console.log("Made it to the end");
-
-} else if (user === 2) {
+else if (user === 2) {
   var calculator = prompt("Would you like to use addition? Press 'a' bruh");
 
   if (calculator === 'a') {
-    alert("Success AAAAA");
+    alert("Welcome to the magic calculator");
     var num2 = parseInt(prompt("Please enter the first number"));
     var num3 = parseInt(prompt("Please enter the second number"));
     var y = num2 + num3;
     alert("The sum of the two numbers you entered is: " + y + " ...magic...");
   }
-
+} else {
+  alert("goodbye");
 }
-
-// #############################################################
-// var numb1 = parseInt(prompt("Enter a number bruh"));
-// var numb2 = parseInt(prompt("Enter another number bruh"));
-//
-// for (var y = numb1; y < numb2; y++){
-//   console.log(y);
-// }
-//
-//
-// var userInput = "";
-// var isUserInputQ = false;
-// while(isUserInputQ) {
-//   userInput = prompt("You entered + " + userInput + ". Print")
-// }
-// alert("You pressed 'q'!!!");
-// #############################################################
